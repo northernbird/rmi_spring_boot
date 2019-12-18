1e649db7d88464fd92c97282cd2e5be8f6a7df26
Rie Miyazaki <miyazaki.rie98@gmail.com>
2019-12-19 08:08:32 +0900

    Chore: Added missing necessary files for the change

--- src/main/java/edu/self/App.java
+++ src/main/java/edu/self/App.java
@@ -8,10 +8,12 @@ import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 //@RestController // Webアプリのリクエストを受け付けるクラスであることの指定
 //@EnableAutoConfiguration // 様々な設定を自動的に有効化
 //@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@EnableSwagger2
 @SpringBootApplication
 public class App {
 //    @RequestMapping("/") // URLのパスの指定
